8ea9d724970dbe2eff9d87c360be31ecdd7d8470
Tylam.dev <tylam.dev@gmail.com>
2024-07-04 16:37:19 -0500

    Creates a db in memory

--- Program.cs
+++ Program.cs
@@ -1,6 +1,18 @@
+using Entity_Framework_Csharp;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDb"));
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) => 
+{
+    dbContext.Database.EnsureCreated();
+    return Results.Ok("Base de datos en memoria" + dbContext.Database.IsInMemory());
+});
 
 app.Run();
