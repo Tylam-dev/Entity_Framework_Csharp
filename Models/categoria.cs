c814fb5c4728d390142154f48a6440c00097ffe7
Tylam.dev <tylam.dev@gmail.com>
2024-07-04 15:44:01 -0500

    Creates models

---
+++ Models/categoria.cs
@@ -0,0 +1,9 @@
+﻿namespace Entity_Framework_Csharp;
+
+public class categoria
+{
+    public Guid categoriaId {get;set};
+    public string Nombre {get;set};
+    public string Descripcion {get;set};
+    public virtual ICollection<Tarea> Tareas {get;set};
+}
